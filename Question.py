5ad1f16ce189a9c3bd28d30cbda4f0f46ad4f0d9
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2022-05-16 11:22:57 +0530

    Create Question.py

---
+++ Question.py
@@ -0,0 +1,58 @@
+20MCA134 ADVANCED DATABASE LAB
+INTERNAL EXAMINATION SEPTEMBER 2021
+Time: 1hr                                                                                                                                   Mark: 15                             
+Create a database named “exam” and create a collection named “student” into it. 
+Insert the following document into the collection student(Using python code)
+_id=1
+Name=Anjali
+Place=Kollam
+Phone=8582639562
+Vaccination_status=”Both vaccinated”
+RTPCR=negative
+Lab_mark=Internal:30,External:45
+Department=MCA
+Insert the following documents into the collection student(Using python code)
+
+_id=2
+Name=Anuradha
+Place=Varkala
+Phone=9992639562
+Vaccination_status=”Both vaccinated”
+RTPCR=negative
+Lab_mark=Internal:40,External:48
+Department=Civil
+_id=3
+Name=Bismiya
+Place=Kollam
+Phone=9446639562
+Vaccination_status=”not vaccinated”
+RTPCR=positive
+Lab_mark=Internal:50,External:39
+Department=MCA
+_id=4
+Name=Vimal
+Place=Ernakulam
+Phone=8582639568
+Vaccination_status=”First dose only”
+RTPCR=positive
+Lab_mark=Internal:40,External:42
+Department=Civil                                                                                                                     P.T.O
+_id=5
+Name=Vivek
+Place=Kollam
+Phone=8582639777
+Vaccination_status=”Both vaccinated”
+RTPCR=negative
+Lab_mark=Internal:50,External:50
+Department=MCA
+Write a NoSQL Query to display the name and phone number of students who are both not vaccinated
+Write a NoSQL Query to display the name and phone number of top two students in MCA department(based on lab external mark)  
+Write a NoSQL Query to display the id, name and department of students whose name starts with ‘A’
+Write a NoSQL Query to update the vaccination status as ‘both vaccinated’ of the student whose id=4
+Write a NoSQL Query to display the name of students in descending order based on the lab external mark.
+
+
+
+
+
+
